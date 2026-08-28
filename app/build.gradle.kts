828ac886eec0839aec2f9ca44a3997566840fea8
WorkerOfArea51 <area.51.ancientworkers@gmail.com>
2026-08-28 14:54:25 +0600

    release: v4.8.160 - OmniArchiver-F2L API Importer & Secret Access Gate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         minSdk = 24
         targetSdk = 35
         val envVersionName = System.getenv("VERSION_NAME")?.removePrefix("v")?.removePrefix("V")
-        versionCode = System.getenv("BUILD_NUMBER")?.toIntOrNull() ?: 158
-        versionName = if (!envVersionName.isNullOrBlank()) envVersionName else "4.8.158"
+        versionCode = System.getenv("BUILD_NUMBER")?.toIntOrNull() ?: 160
+        versionName = if (!envVersionName.isNullOrBlank()) envVersionName else "4.8.160"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
